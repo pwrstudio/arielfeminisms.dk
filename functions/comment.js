30d0236476101c8cfd74778b162e2bc20634ffc8
pwrstudio <cell@pwr.site>
2020-02-01 13:21:32 +0100

    Test comment

--- functions/comment.js
+++ functions/comment.js
@@ -7,7 +7,6 @@
 
 // https://docs.netlify.com/functions/build-with-javascript/#runtime-settings
 
-
 // {
 //     "path": "Path parameter",
 //     "httpMethod": "Incoming request's method name"
@@ -17,43 +16,53 @@
 //     "isBase64Encoded": "A boolean flag to indicate if the applicable request payload is Base64-encode"
 // }
 
-// client
-//   .patch('bike-123') // Document ID to patch
-//   .set({inStock: false}) // Shallow merge
-//   .inc({numSold: 1}) // Increment field by count
-//   .commit() // Perform the patch and return a promise
-//   .then(updatedBike => {
-//     console.log('Hurray, the bike is updated! New document:')
-//     console.log(updatedBike)
-//   })
-//   .catch(err => {
-//     console.error('Oh no, the update failed: ', err.message)
-//   })
+const get = require('lodash / get');
 
 const sanityClient = require('@sanity/client')
 const client = sanityClient({
     projectId: '883bmkra',
     dataset: 'production',
-    token: process.env.SANITY_TOKEN, // or leave blank to be anonymous user
-    useCdn: true // `false` if you want to ensure fresh data
+    token: process.env.SANITY_TOKEN
 })
 
 exports.handler = function (event, context, callback) {
 
     const { identity, user } = context.clientContext;
 
-    if (!user) {
-        callback(
-            null, {
-            statusCode: 401,
-            body: 'Unauthorized'
-        });
-    } else {
+    const doc = {
+        _type: 'ygrgComment',
+        title: 'test comment',
+        author: get(user, 'sub', 'XXX'),
+        content: event.queryStringParameters.comment,
+        textReference: {
+            _type: 'reference',
+            _ref: '76bddb66-4972-4e7d-8cb1-14cb78a2a01f'
+        }
+    }
+
+    client.create(doc).then(res => {
+        console.log(`Comment was created, document ID is ${res._id}`)
         callback(
             null, {
             statusCode: 200,
-            body: JSON.stringify({ user: user, event: event })
+            body: JSON.stringify(res)
         });
-    }
+    })
+
+
+
+    // if (!user) {
+    //     callback(
+    //         null, {
+    //         statusCode: 401,
+    //         body: 'Unauthorized'
+    //     });
+    // } else {
+    // callback(
+    //     null, {
+    //     statusCode: 200,
+    //     body: JSON.stringify({ user: user, event: event })
+    // });
+    // }
 
 }
