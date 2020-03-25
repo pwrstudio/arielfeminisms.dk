6b2bbeb1fb9ce8c79a2ab17b4761fd5548c49bdb
pwrstudio <cell@pwr.site>
2020-03-25 20:12:17 +0100

    Avatar 3

--- functions/avatar.js
+++ functions/avatar.js
@@ -6,24 +6,16 @@
 
 const get = require('lodash/get');
 
-const sanityClient = require('@sanity/client')
-const client = sanityClient({
-    projectId: '883bmkra',
-    dataset: 'production',
-    token: process.env.SANITY_TOKEN
-})
+// const sanityClient = require('@sanity/client')
+// const client = sanityClient({
+//     projectId: '883bmkra',
+//     dataset: 'production',
+//     token: process.env.SANITY_TOKEN
+// })
 
 exports.handler = function (event, context, callback) {
 
     const { user } = context.clientContext;
-
-    console.dir(event)
-
-    callback(
-        null, {
-        statusCode: 200,
-        body: JSON.stringify(user)
-    });
 
     if (!user) {
         callback(
@@ -33,31 +25,22 @@ exports.handler = function (event, context, callback) {
         });
     } else {
 
-        // const commentContent = get(event, 'queryStringParameters.comment', "")
-        // const title = get(event, 'queryStringParameters.title', "")
-        // const location = get(event, 'queryStringParameters.location', 0)
-        // const userName = get(user, 'user_metadata.name', '')
-        // const userId = get(user, 'sub', 'No ID')
-
-        // Upload an image file from the file system
-        // client.assets
-        //     .upload('image', fs.createReadStream('myImage.jpg'))
-        //     .then(document => {
-        //         console.log('The image was uploaded!', document)
-        //         callback(
-        //             null, {
-        //             statusCode: 200,
-        //             body: JSON.stringify(document)
-        //         });
-        //     })
-        //     .catch(error => {
-        //         console.error('Upload failed:', error.message)
-        //         callback(
-        //             null, {
-        //             statusCode: 500,
-        //             body: error.message
-        //         });
-        //     })
+        user
+            .update({ data: { avatar: 'XXX' } })
+            .then(user => {
+                callback(
+                    null, {
+                    statusCode: 200,
+                    body: JSON.stringify(event)
+                });
+            })
+            .catch(error => {
+                callback(
+                    null, {
+                    statusCode: 500,
+                    body: JSON.stringify(event)
+                });
+            });
 
     }
 }
