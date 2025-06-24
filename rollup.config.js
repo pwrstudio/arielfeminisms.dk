3470b741bcc0afcf1255a36245adf694c8e0436f
PWR <pwrpntr@gmail.com>
2025-06-24 18:23:15 +0200

    mvp update of build system

--- rollup.config.js
+++ rollup.config.js
@@ -2,9 +2,10 @@ import svelte from 'rollup-plugin-svelte';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
+import terser from '@rollup/plugin-terser';
 import sveltePreprocess from 'svelte-preprocess';
-import babel from 'rollup-plugin-babel';
+import babel from '@rollup/plugin-babel';
+import css from 'rollup-plugin-css-only';
 
 const production = !process.env.ROLLUP_WATCH;
 
@@ -18,15 +19,17 @@ export default {
 	},
 	plugins: [
 		svelte({
-			// enable run-time checks when not in production
-			dev: !production,
-			// we'll extract any component CSS out into
-			// a separate file — better for performance
-			css: css => {
-				css.write('public/build/bundle.css');
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production,
 			},
+			// extract component CSS out into a separate file
+			emitCss: true,
 			preprocess: sveltePreprocess()
 		}),
+		
+		// Extract component CSS into a separate file
+		css({ output: 'bundle.css' }),
 
 		// If you have external dependencies installed from
 		// npm, you'll most likely need these plugins. In
@@ -97,10 +100,10 @@ function serve() {
 			if (!started) {
 				started = true;
 
-				require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				import('child_process').then(cp => cp.spawn('npm', ['run', 'start', '--', '--dev'], {
 					stdio: ['ignore', 'inherit', 'inherit'],
 					shell: true
-				});
+				}));
 			}
 		}
 	};
